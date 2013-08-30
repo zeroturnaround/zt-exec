80a5e3185b5c8ebaa06935e83f18685e91224a95
Toomas Romer <toomas@zeroturnaround.com>
2013-08-30 13:02:07 +0300

    Added a test

---
+++ src/test/java/org/zeroturnaround/exec/test/PrintInputToOutput.java
@@ -0,0 +1,20 @@
+package org.zeroturnaround.exec.test;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class PrintInputToOutput {
+  public static void main(String[] args) throws Exception {
+    InputStreamReader isr = new InputStreamReader(System.in);
+    BufferedReader br = new BufferedReader(isr);
+
+    String line = null;
+    int count = 0;
+    while ((line = br.readLine()) != null) {
+      System.out.println(line);
+      count++;
+      if (count == 3)
+        break;
+    }
+  }
+}
