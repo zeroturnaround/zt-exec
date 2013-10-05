849795eccdedf95324cc8979ab178f9260681bc9
Rein Raudjärv <rein@zeroturnaround.com>
2013-10-05 17:55:54 +0300

    Added ProcessExecutorShutdownHookTest.

---
+++ src/test/java/org/zeroturnaround/exec/test/shutdown/ProcessExecutorShutdownHookTest.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2013 ZeroTurnaround <support@zeroturnaround.com>
+ * Contains fragments of code from Apache Commons Exec, rights owned
+ * by Apache Software Foundation (ASF).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.zeroturnaround.exec.test.shutdown;
+
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang.SystemUtils;
+import org.junit.Assert;
+import org.junit.Test;
+import org.zeroturnaround.exec.ProcessExecutor;
+
+
+/**
+ * Tests destroying processes on JVM exit.
+ */
+public class ProcessExecutorShutdownHookTest {
+
+  private static final long SLEEP_FOR_RECHECKING_FILE = 2000;
+
+  @Test
+  public void testDestroyOnExit() throws Exception {
+    File file = WriterLoop.getFile();
+    if (file.exists())
+      FileUtils.forceDelete(file);
+    new ProcessExecutor("java", "-cp", SystemUtils.JAVA_CLASS_PATH, WriterLoopStarter.class.getName()).redirectOutputAsInfo().execute();
+    // After WriterLoopStarter has finished we expect that WriterLoop is also finished - no-one is updating the file
+    checkFileStaysTheSame(file);
+    FileUtils.forceDelete(file);
+  }
+
+  private static void checkFileStaysTheSame(File file) throws InterruptedException {
+    Assert.assertTrue(file.exists());
+    long length = file.length();
+    Thread.sleep(SLEEP_FOR_RECHECKING_FILE);
+    Assert.assertEquals("File '" + file + "' was still updated.", length, file.length());
+  }
+
+}
---
+++ src/test/java/org/zeroturnaround/exec/test/shutdown/WriterLoop.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2013 ZeroTurnaround <support@zeroturnaround.com>
+ * Contains fragments of code from Apache Commons Exec, rights owned
+ * by Apache Software Foundation (ASF).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.zeroturnaround.exec.test.shutdown;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
+/**
+ * Program which regularly writes into a file.
+ * By checking whether the file gets updates we know whether this program is still running or it's finished.
+ */
+public class WriterLoop {
+
+  private static final File FILE = new File("writeLoop.data");
+
+  private static final long INTERVAL = 1000;
+  private static final long COUNT = 10;
+
+  public static File getFile() {
+    return FILE;
+  }
+
+  public static void main(String[] args) throws Exception {
+    PrintWriter out = new PrintWriter(new FileWriter(FILE), true);
+    try {
+      out.println("Started");
+      for (int i = 0; i < COUNT; i++) {
+        out.println(i);
+        Thread.sleep(INTERVAL);
+      }
+      out.println("Finished");
+    }
+    finally {
+      out.close();
+    }
+  }
+
+}
