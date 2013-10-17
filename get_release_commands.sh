263f41b1c139d8f30f4b05993d0db611eeaa7170
Toomas Romer <toomas@zeroturnaround.com>
2013-10-17 10:13:49 +0200

    Updated the release script file with proper project name

--- get_release_commands.sh
+++ get_release_commands.sh
@@ -3,7 +3,7 @@
 
 if [[ "$1" = "-h" ]] || [[ "$1" = "--help" ]]; then
   echo "Usage: './get_release_commands.sh VERSION_TO_RELEASE [NEW_DEV_VERSION]'"
-  echo "Prints out commands to run to release zt-zip to Maven Central."
+  echo "Prints out commands to run to release zt-exec to Maven Central."
   echo "VERSION_TO_RELEASE must be a number (integer or floating). NEW_DEV_VERSION is optional"
   exit 0
 fi
@@ -30,7 +30,7 @@ else
   echo "New development version will be ${new_version}"
 fi
 
-echo "To release zt-zip version ${version} run these commands:"
+echo "To release zt-exec version ${version} run these commands:"
 echo ""
 echo "1) set release version"
 echo "mvn versions:set -DnewVersion=${version}"
@@ -38,8 +38,8 @@ echo "mvn versions:set -DnewVersion=${version}"
 echo ""
 echo "2) commit & tag"
 echo "git add pom.xml; "
-echo "git commit -m \"Prepare release zt-zip-${version}\""
-echo "git tag zt-zip-${version}"
+echo "git commit -m \"Prepare release zt-exec-${version}\""
+echo "git tag zt-exec-${version}"
 
 echo ""
 echo "3) build release"
@@ -55,15 +55,15 @@ echo "mvn source:jar"
 
 echo ""
 echo "6) deploy and sign releases archive"
-echo "mvn gpg:sign-and-deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=sonatype-nexus-staging -DpomFile=pom.xml -Dfile=target/zt-zip-$1.jar"
+echo "mvn gpg:sign-and-deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=sonatype-nexus-staging -DpomFile=pom.xml -Dfile=target/zt-exec-$1.jar"
 
 echo ""
 echo "7) deploy and sign sources archive"
-echo "mvn gpg:sign-and-deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=sonatype-nexus-staging -DpomFile=pom.xml -Dfile=target/zt-zip-$1-sources.jar -Dclassifier=sources"
+echo "mvn gpg:sign-and-deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=sonatype-nexus-staging -DpomFile=pom.xml -Dfile=target/zt-exec-$1-sources.jar -Dclassifier=sources"
 
 echo ""
 echo "8) deploy and sign javadoc archive"
-echo "mvn gpg:sign-and-deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=sonatype-nexus-staging -DpomFile=pom.xml -Dfile=target/zt-zip-$1-javadoc.jar -Dclassifier=javadoc"
+echo "mvn gpg:sign-and-deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=sonatype-nexus-staging -DpomFile=pom.xml -Dfile=target/zt-exec-$1-javadoc.jar -Dclassifier=javadoc"
 
 echo ""
 echo "9) set new development version"
