1b03f726a126504cae64d5996ea9ca6499913da0
Rein Raudjärv <rein@zeroturnaround.com>
2015-10-07 17:08:33 +0300

    Added EmptyArgTest.

---
+++ src/test/java/org/zeroturnaround/exec/test/EmptyArgTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2014 ZeroTurnaround <support@zeroturnaround.com>
+ * Contains fragments of code from Apache Commons Exec, rights owned
+ * by Apache Software Foundation (ASF).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.zeroturnaround.exec.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.zeroturnaround.exec.ProcessExecutor;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Tests passing empty arguments.
+ *
+ * @see ProcessExecutor
+ * @see ArgumentsAsList
+ */
+public class EmptyArgTest {
+
+  @Test
+  public void testReadOutputAndError() throws Exception {
+    String output = argumentsAsList("arg1", "", "arg3", "").readOutput(true).execute().outputUTF8();
+    Assert.assertEquals("[arg1, , arg3, ]", output);
+  }
+
+  private ProcessExecutor argumentsAsList(String... args) {
+    List<String> command = new ArrayList<String>();
+    command.addAll(Arrays.asList("java", "-cp", "target/test-classes", ArgumentsAsList.class.getName()));
+    command.addAll(Arrays.asList(args));
+    return new ProcessExecutor(command);
+  }
+
+}
